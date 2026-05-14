e2510716ef6117ee911c899e20e5dc240aa2f271
WSebastianML <wsebastianmoyano@gmail.com>
2026-05-14 11:31:43 -0500

    feat(importacionesapp): mostrar cuentas de importaciones

--- importacionesapp/views.py
+++ importacionesapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from core.db_context import get_db_from_request
 from core.context_processors import company_context
 from comprasapp.services import OrdenComprasService
 from comprasapp.serializers import(
-    DivisionSerializer, TipoCreditoSerializer, SolicitanteSerializer
+    DivisionSerializer, TipoCreditoSerializer, SolicitanteSerializer, CuentaSerializer
 )
 
 @require_http_methods(["GET"])
@@ -28,6 +29,19 @@ def ordenImportacion(request):
         'username': company_context(request).get('db_user', ''),
         'company_key': company_key,
     })
+
+@require_http_methods(["GET"])
+def cuentaProv(request):
+    db_alias = get_db_from_request(request)
+    service = OrdenComprasService()
+
+    cuentas_proveedor = service.get_cuentas_importaciones(db_alias=db_alias)
+    if not cuentas_proveedor:
+        return JsonResponse({'error': 'No se encontraron cuentas para el proveedor'})
+    
+    cuentas_proveedor_data = CuentaSerializer(cuentas_proveedor, many = True).data
+    
+    return JsonResponse({'ncuentaprov': cuentas_proveedor_data})
 
 
 
